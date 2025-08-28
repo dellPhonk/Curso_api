1710dc2c4367c6c9ec91c6f79bb1f0b22fd46154
dellPhonk <cmcalapica@gmail.com>
2025-08-28 19:53:12 -0300

    comitando02

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Helo Word')
+import express from 'express';
+
+const servidor = express();
+
+servidor.get('/helloword', (req, resp) => { 
+    resp.send('ola mundo');
+})
+
+servidor.get('/mensagem/boasvinda', (req,resp) => {
+    resp.send('ola sem bem vindes divos');
+})
+
+servidor.get('/v2/mensagem/boasvinda', (req,resp) => {
+    resp.send('salve bandidagem, malandragi vive');
+})
+
+servidor.get('/calculadora/somar/:n1/:n2', (req,resp) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+
+    let calc = n1 + n2;
+    resp.send(`a soma de ${n1} + ${n2} é ${calc}`);
+})
+
+servidor.listen(5010, () => console.log('Api subida com sucesso na porta 5010!'));
